dac93a031b4e9ceabfe6a706e6b0008e8dbb60ad
Muhammad Rubiyanto Permana <rubiyanto.permana@gmail.com>
2017-06-08 15:04:19 +0700

    Initial Commit
    initial Commit

---
+++ src/main/java/polban/jtk/absensimahasiswa/JadwalMataKuliahMicroservicesServer/JadwalMataKuliahMicroservicesServerApplication.java
@@ -0,0 +1,12 @@
+package polban.jtk.absensimahasiswa.JadwalMataKuliahMicroservicesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadwalMataKuliahMicroservicesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadwalMataKuliahMicroservicesServerApplication.class, args);
+	}
+}
---
+++ src/test/java/polban/jtk/absensimahasiswa/JadwalMataKuliahMicroservicesServer/JadwalMataKuliahMicroservicesServerApplicationTests.java
@@ -0,0 +1,16 @@
+package polban.jtk.absensimahasiswa.JadwalMataKuliahMicroservicesServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JadwalMataKuliahMicroservicesServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
